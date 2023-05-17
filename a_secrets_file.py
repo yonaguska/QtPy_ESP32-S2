33726ac984edbde72c704b5f4a5126071bef7de8
yonaguska <kirkaf@yonaguska.com>
2023-05-17 15:07:58 -0500

    Create a_secrets_file.py
    Show the construction details of a secrets.py file

---
+++ a_secrets_file.py
@@ -0,0 +1,18 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : ’YOUR_WIFI_SSID’,
+    'password' : ‘YOUR _SSID_PASSWORD,
+
+    # 'broker' : 'io.adafruit.com',
+    'port' : 1883,
+    #'aio_username' : “YOUR_ADAFRUIT_IO_USERNAME”,
+    #'aio_key' : ‘YOUR_ADAFRUIT_IO_KEY”,
+    'location' : ‘CITY, STATE COUNTRY’,
+
+
+    'broker' : ‘YOUR_MQTT_BROKER_IP’,
+    #'user' : ‘YOUR’_MQTT_USERNAME,
+    #'pass' : ‘YOUR_MQTT_PASSWORD’,
+}
